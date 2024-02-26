c4213433576f1b4d4d2641e563b2027798506069
custom_dev <email@email.com>
2024-02-26 17:55:21 +0400

    init commit

---
+++ mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QtConcurrent>
+#include "ExampleRace.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void StartRace( void );
+
+
+private slots:
+
+    void on_pb_start_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    uint32_t number = 0, countFinish = 0;
+
+    QMutex m;
+    Controller *race1;
+    Controller *race2;
+
+    ExampleRace *concurRace1;
+    ExampleRace *concurRace2;
+
+    QFuture<void> futrace1;
+    QFutureWatcher<void> whFutrace1;
+    QFuture<void> futrace2;
+    QFutureWatcher<void> whFutrace2;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // MAINWINDOW_H
